55df0b5aed6a6eb47bea6a92cb94940e4be7b2ca
notomo <notomo.motono@gmail.com>
2023-10-09 14:08:57 +0900

    Quit vendoring test assertlib

--- lua/runtimetable/test/helper.lua
+++ lua/runtimetable/test/helper.lua
@@ -2,6 +2,9 @@ local helper = require("vusted.helper")
 local plugin_name = helper.get_module_root(...)
 
 helper.root = helper.find_plugin_root(plugin_name)
+vim.opt.packpath:prepend(vim.fs.joinpath(helper.root, "spec/.shared/packages"))
+require("assertlib").register(require("vusted.assert").register)
+
 helper.runtimepath = vim.o.runtimepath
 
 function helper.before_each()
@@ -14,9 +17,5 @@ function helper.after_each()
   helper.cleanup_loaded_modules(plugin_name)
   helper.test_data:teardown()
 end
-
-local asserts = require("vusted.assert").asserts
-local asserters = require(plugin_name .. ".vendor.assertlib").list()
-require(plugin_name .. ".vendor.misclib.test.assert").register(asserts.create, asserters)
 
 return helper
